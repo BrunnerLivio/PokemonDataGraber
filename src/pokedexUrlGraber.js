ff7e0f0e87dfdb46dfcabc9984e8cbe100a0f934
gman-php <ich@andy-hofmann.com>
2017-01-03 22:03:12 +0100

    Use condensed version of pokedex for faster page load

--- src/pokedexUrlGraber.js
+++ src/pokedexUrlGraber.js
@@ -6,7 +6,7 @@ class PokedexUrlGraber {
         this.logger = logger;
         return new Promise((resolve, reject) => {
             request({
-                uri: 'https://fevgames.net/pokedex'
+                uri: 'https://fevgames.net/pokemon-go/pokedex-table/'
             }, (error, response, body) => {
                 if (error) {
                     reject(err); return;
@@ -15,7 +15,7 @@ class PokedexUrlGraber {
                 let urls = [];
                 let $ = cheerio.load(body);
 
-                $(".pokedex-item").each((index, element) => {
+                $(".pokedexTable > tbody > tr > td:nth-of-type(2) > a").each((index, element) => {
                     var url = $(element).attr('href');
                     this.logger('| Found ' + url);
                     urls.push(url);
